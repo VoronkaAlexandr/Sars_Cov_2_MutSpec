db4eb1870a8c74f40e1012809f5f335f045ed683
VoronkaAlexandr <57066102+VoronkaAlexandr@users.noreply.github.com>
2021-03-18 20:35:05 +0200

    Add files via upload

---
+++ Sars_Cov_2/cod/05.MutSpecPerDate.R
@@ -0,0 +1,18 @@
+df = read.csv('D:/Sars_Cov_2_MutSpec-main/Sars_Cov_2_MutSpec-main/Sars_Cov_2/data/norm_data_modernized_nextstrain.csv')
+
+only_mut = subset(df, RefNuc != '-' & AltNuc != '-')
+only_mut$NucSubst = paste(only_mut$RefNuc,only_mut$AltNuc,sep='>')
+
+
+for (x in unique(only_mut$Year)){
+  year_set = subset(only_mut, Year == x)
+  for (i in unique(year_set$Month)){
+    temporary = subset(year_set, Month == i)
+    Month12Comp = aggregate(temporary$MutCount, by = list(temporary$NucSubst), FUN = sum);
+    names(Month12Comp)=c('NucSubst','Month_Mut_Count')
+    png(sprintf('D:/Sars_Cov_2_MutSpec-main/Sars_Cov_2_MutSpec-main/Sars_Cov_2/R_figure/Mut_Per_Date/05.Month_MutSpec12_%s.png', paste(x,i,sep='-')))
+    barplot(Month12Comp$Month_Mut_Count, names =  Month12Comp$NucSubst, main = sprintf('Covid Mut spec 12 components at %s', paste(x,i,sep='-')), cex.names = 0.7)
+    dev.off()
+    write.csv(x = Month12Comp, sprintf('D:/Sars_Cov_2_MutSpec-main/Sars_Cov_2_MutSpec-main/Sars_Cov_2/data_obtained/MutPerMonth/05.Month_MutSpec12_%s.csv', paste(x,i,sep='-')))
+  }
+}
